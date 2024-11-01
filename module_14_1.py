4a991923e002e733abc162519c67ccb65f293eb1
MaxHaunter777 <maxhaunter@gmail.com>
2024-11-01 21:49:03 +0300

    Update module_14_1.py

--- module_14_1.py
+++ module_14_1.py
@@ -3,6 +3,7 @@ import sqlite3
 connection = sqlite3.connect('not_telegram.db')
 cursor = connection.cursor()
 
+#Создайте таблицу Users
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Users(
 id INTEGER PRIMARY KEY,
@@ -12,18 +13,24 @@ age INTEGER,
 balance INTEGER NOT NULL
 )
 ''')
-for i in range(10):
-    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)', (f'User{i+1}', f'example{i+1}@gmail.com', f'{(i+1)*10}', '1000'))
 
+#Заполните её 10 записями:
+for i in range(10):
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)', 
+                                    (f'User{i+1}', f'example{i+1}@gmail.com', f'{(i+1)*10}', '1000'))
+
+#Обновите balance у каждой 2ой записи начиная с 1ой на 500:
 for i in range(10):
     if (i+1)%2 != 0:
         cursor.execute('UPDATE Users SET balance = ? WHERE id = ?', (500, f'{i+1}'))
 
+#Удалите каждую 3ую запись в таблице начиная с 1ой:
 i = range(1, 11)
 n = i[::3]
 for i in n:
     cursor.execute('DELETE from users WHERE id = ?', (f'{i}',))
 
+#Сделайте выборку всех записей при помощи fetchall()
 cursor.execute('SELECT * FROM Users')
 cursor.execute('SELECT username, email, age, balance from users WHERE age != ?', (60,))
 users = cursor.fetchall()
