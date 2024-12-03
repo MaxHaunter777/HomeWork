c65f6a4f8c02213f551ca48a9a317d1b2a135e6b
MaxHaunter777 <maxhaunter@gmail.com>
2024-12-03 22:42:34 +0300

    Update module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -26,7 +26,7 @@ async def create_user(username: Annotated[str, Path(min_length=3, max_length=20,
 # "Имя: {username}, возраст: {age}". И возвращает строку "The user <user_id> is updated"
 @app.put("/user/{user_id}/{username}/{age}")
 async def update_user(user_id: Annotated[int, Path(ge=1, le=100, title="User ID", description="Enter User ID", example="1")],
-                      username: Annotated[str, Path(min_length=3, max_length=20, description="Enter username, example="UrbanUser"")],
+                      username: Annotated[str, Path(min_length=3, max_length=20, description="Enter username", example="UrbanUser")],
                       age: Annotated[int, Path(ge=18, le=120, description="Enter age",  example="24")]) -> str:
     users[str(user_id)] = f"Имя: {username}, возраст: {age}"
     return f"The user {user_id} is updated"
