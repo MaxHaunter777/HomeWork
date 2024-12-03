20462d62a11c9ff8600f3f0756aee85d8d02b27a
MaxHaunter777 <maxhaunter@gmail.com>
2024-12-03 21:47:33 +0300

    Update module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -15,8 +15,8 @@ async def get_users() -> dict:
 # post запрос по маршруту '/user/{username}/{age}', который добавляет в словарь по максимальному по значению ключом значение строки
 # "Имя: {username}, возраст: {age}". И возвращает строку "User <user_id> is registered".
 @app.post("/user/{username}/{age}")
-async def create_user(username: Annotated[str, Path(min_length=3, max_length=20, description="Enter username")],
-                      age: Annotated[int, Path(ge=18, le=120, description="Enter age")]) -> str:
+async def create_user(username: Annotated[str, Path(min_length=3, max_length=20, description="Enter username", example="UrbanUser")],
+                      age: Annotated[int, Path(ge=18, le=120, description="Enter age",  example="24")]) -> str:
     user_id = str(int(max(users, key=int)) + 1)
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"User {user_id} is registered"
@@ -25,14 +25,14 @@ async def create_user(username: Annotated[str, Path(min_length=3, max_length=20,
 # который обновляет значение из словаря users под ключом user_id на строку
 # "Имя: {username}, возраст: {age}". И возвращает строку "The user <user_id> is updated"
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: Annotated[int, Path(ge=1, le=100, title="User ID", description="Enter User ID")],
-                      username: Annotated[str, Path(min_length=3, max_length=20, description="Enter username")],
-                      age: Annotated[int, Path(ge=18, le=120, description="Enter age")]) -> str:
+async def update_user(user_id: Annotated[int, Path(ge=1, le=100, title="User ID", description="Enter User ID", example="1")],
+                      username: Annotated[str, Path(min_length=3, max_length=20, description="Enter username, example="UrbanUser"")],
+                      age: Annotated[int, Path(ge=18, le=120, description="Enter age",  example="24")]) -> str:
     users[str(user_id)] = f"Имя: {username}, возраст: {age}"
     return f"The user {user_id} is updated"
 
 #delete запрос по маршруту '/user/{user_id}', который удаляет из словаря users по ключу user_id пару.
 @app.delete("/user/{user_id}")
-async def delete_user(user_id: Annotated[int, Path(ge=1, le=100, title="User ID", description="Enter User ID")]) -> str:
+async def delete_user(user_id: Annotated[int, Path(ge=1, le=100, title="User ID", description="Enter User ID", example="1")]) -> str:
     users.pop(str(user_id))
     return f"The user {user_id} has been deleted"
